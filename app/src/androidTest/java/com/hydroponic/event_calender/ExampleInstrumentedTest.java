28bd0a40ad9a883704cc398d0ba4773f5381b373
hydroponic <itachirules11@gmail.com>
2023-04-19 12:53:22 +0300

    Корректировка активностей

--- app/src/androidTest/java/com/hydroponic/event_calender/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/hydroponic/event_calender/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.rezwan2525.event_calender_mvvm_room", appContext.getPackageName());
+        assertEquals("com.hydroponic.event_calender", appContext.getPackageName());
     }
 }
